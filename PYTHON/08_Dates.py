b63c25e77af6f2c51c820bf240beefc378a4d85a
walid gzouly <wagzouly@gmail.com>
2024-05-14 15:38:26 +0100

    101-PY

---
+++ PYTHON/08_Dates.py
@@ -0,0 +1,98 @@
+# --------------------------------------------------------------------#
+# Dates :
+
+# Python has a module named datetime to work with dates and times.
+# Import the datetime module and display the current date:
+# import datetime
+# --------------------------------------------------------------------#
+# import datetime
+
+import datetime
+
+x = datetime.datetime.now()
+
+y = x.strftime("%a")
+
+a = x.strftime("%A")
+
+b = x.strftime("%B")
+
+c = x.strftime("%Y")
+
+z = x.strftime("%d")
+
+print(y)
+
+print(z, a, b, c)
+
+# --------------------------------------------------------------------#
+
+# The date contains year, month, day, hour, minute, second, and microsecond.
+
+# The datetime module has many methods to return information about the date object.
+
+# Here are a few examples, you will learn more about them later in this chapter:
+
+# stftime() method to format date objects into readable strings:
+
+# import datetime
+
+# x = datetime.datetime.now()
+
+# print(x.strftime("%a"))
+
+# --------------------------------------------------------------------#
+
+# A reference of all the legal format codes:
+
+# Directive	Description	Example
+
+# %a	Weekday, short version	Wed
+
+# %A	Weekday, full version	Wednesday
+
+# %w	Weekday as a number 0-6, 0 is Sunday	3
+
+# %d	Day of month 01-31	31
+
+# %b	Month name, short version	Dec
+
+# %B	Month name, full version	December
+
+# %m	Month as a number 01-12	12
+
+# %y	Year, short version, without century	18
+
+# %Y	Year, full version	2018
+
+# %H	Hour 00-23	17
+
+# %I	Hour 00-12	05
+
+# %p	AM/PM	PM
+
+# %M	Minute 00-59	41
+
+# %S	Second 00-59	08
+
+# %f	Microsecond 000000-999999	548513
+
+# %z	UTC offset	+0100
+
+# %Z	Timezone	CST
+
+# %j	Day number of year 001-366	365
+
+# %U	Week number of year, Sunday as the first day of week, 00-53	52
+
+# %W	Week number of year, Monday as the first day of week, 00-53	52
+
+# %c	Local version of date and time	Mon Dec 31 17:41:00 2018
+
+# %x	Local version of date	12/31/18
+
+# %X	Local version of time	17:41:00
+
+# %%%	A % character	%
+
+# --------------------------------------------------------------------#
